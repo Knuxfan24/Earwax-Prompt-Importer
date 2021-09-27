ac0f01879edeccecf29c2b854e65550b352d9188
Knux <knuxfan@live.co.uk>
2021-09-27 11:23:25 +0100

    Support Audio Importing
    Makes the project name slightly incorrect. FUN!

--- Earwax Prompt Importer/Program.cs
+++ Earwax Prompt Importer/Program.cs
@@ -32,6 +32,34 @@ namespace Earwax_Prompt_Importer
 
             public override string ToString() => Name;
         }
+        public class AudioFormatData
+        {
+            [JsonProperty(Order = 1)]
+            public int episodeid { get; set; }
+
+            [JsonProperty(Order = 2)]
+
+            public List<EarwaxAudio> Content = new();
+        }
+        public class EarwaxAudio
+        {
+            [JsonProperty(Order = 1)]
+            public bool X { get; set; }
+
+            [JsonProperty(Order = 2)]
+            public string Name { get; set; }
+
+            [JsonProperty(Order = 3)]
+            public string Short { get; set; }
+
+            [JsonProperty(Order = 4)]
+            public int ID { get; set; }
+
+            [JsonProperty(Order = 5)]
+            public List<string> Categories = new();
+
+            public override string ToString() => Name;
+        }
 
         static void Main(string[] args)
         {
@@ -46,77 +74,181 @@ namespace Earwax_Prompt_Importer
             // Split text file.
             var text = File.ReadAllLines(args[0]);
 
-            // Set up the actual Data.
-            PromptFormatData Data = new();
-
-            // Loop through each entry in our text file.
-            for (int i = 0; i < text.Length; i++)
+            // Check if this is an audio list.
+            if (text[0] == "[Audio]")
             {
-                EarwaxPrompt prompt = new()
+                // Create the directories to store things in.
+                // TODO, maybe see about checking if these already exist and asking for permission to delete them.
+                Directory.CreateDirectory($"{Path.GetDirectoryName(args[0])}\\CustomEarwaxSounds");
+                Directory.CreateDirectory($"{Path.GetDirectoryName(args[0])}\\CustomEarwaxSounds\\Oggs");
+                Directory.CreateDirectory($"{Path.GetDirectoryName(args[0])}\\CustomEarwaxSounds\\SpectrumDummies");
+
+                // Set up the actual Data.
+                AudioFormatData AudioData = new()
                 {
-                    ID = 24000000 + i,
-                    X = false,
-                    PromptAudio = $"custom_{i}",
-                    Name = text[i]
+                    episodeid = 1234
                 };
 
-                Data.Content.Add(prompt);
-
-                // Create the WAV for the user to manually make into OGGs (src: https://docs.microsoft.com/en-us/dotnet/api/system.speech.synthesis.speechsynthesizer.setoutputtowavefile?view=netframework-4.8)
-                // Initialize a new instance of the SpeechSynthesizer.  
-                SpeechSynthesizer synth = new();
-
-                // Configure the audio output.   
-                synth.SetOutputToWaveFile($@"{Path.GetDirectoryName(args[0])}\custom_{i}.wav");
-
-                // Build a prompt, strip out the control tags.  
-                PromptBuilder builder = new();
-                string tts = text[i].Replace("<ANY>", "this player");
-                tts = tts.Replace("<i>", "");
-                tts = tts.Replace("</i>", "");
-                builder.AppendText(tts);
-
-                // Speak the sound to our WAV file.
-                synth.Speak(builder);
-
-                // Dispose of the builder so we can actually delete the WAV afterwards.
-                synth.Dispose();
-
-                // Convert WAV to OGG
-                if (File.Exists($@"{Path.GetDirectoryName(args[0])}\custom_{i}.wav"))
+                // Loop through our list of sounds.
+                for (int i = 1; i < text.Length; i++)
                 {
-                    using (Process process = new())
+                    // Check this line actually contains the split character.
+                    if (text[i].Contains('|'))
                     {
-                        process.StartInfo.FileName = $"\"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\\ExternalResources\\oggenc2.exe\"";
-                        process.StartInfo.Arguments = $"\"{Path.GetDirectoryName(args[0])}\\custom_{i}.wav\"";
-                        process.StartInfo.UseShellExecute = false;
-                        process.StartInfo.RedirectStandardOutput = true;
-                        process.StartInfo.CreateNoWindow = true;
+                        // Split the string on the split character
+                        string[] split = text[i].Split('|');
 
-                        process.Start();
-                        process.BeginOutputReadLine();
-                        process.WaitForExit();
+                        // Check the specified file exists.
+                        if (File.Exists(split[0]))
+                        {
+                            Console.WriteLine($"Converting '{split[0]}' to Earwax sound.");
+
+                            EarwaxAudio audio = new()
+                            {
+                                X = false,
+                                Name = split[1],
+                                Short = split[1],
+                                ID = 24000000 + i
+                            };
+                            AudioData.Content.Add(audio);
+
+                            // If this file's already an OGG, then copy it, if not then convert it.
+                            // TODO, test more formats than just WAVs.
+                            if (Path.GetExtension(split[0]) == ".ogg")
+                            {
+                                File.Copy(split[0], $"{Path.GetDirectoryName(args[0])}\\CustomEarwaxSounds\\Oggs\\{audio.ID}.ogg", true);
+                            }
+                            else
+                            {
+                                using (Process process = new())
+                                {
+                                    process.StartInfo.FileName = $"\"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\\ExternalResources\\oggenc2.exe\"";
+                                    process.StartInfo.Arguments = $"-o \"{Path.GetDirectoryName(args[0])}\\CustomEarwaxSounds\\Oggs\\{audio.ID}.ogg\" \"{split[0]}\"";
+                                    process.StartInfo.UseShellExecute = false;
+                                    process.StartInfo.RedirectStandardOutput = true;
+                                    process.StartInfo.CreateNoWindow = true;
+
+                                    process.Start();
+                                    process.BeginOutputReadLine();
+                                    process.WaitForExit();
+                                }
+                            }
+
+                            // Sloppily create a dummy spectrum file so the game doesn't hang.
+                            List<string> DummySpectrum = new();
+                            DummySpectrum.Add("{");
+                            DummySpectrum.Add("\t\"Refresh\":23,");
+                            DummySpectrum.Add("\t\"Frequencies\":[");
+                            DummySpectrum.Add("\t\t{\"left\":[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],");
+                            DummySpectrum.Add("\t\t \"right\":[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}");
+                            DummySpectrum.Add("\t],");
+                            DummySpectrum.Add("\t\"Peak\":100");
+                            DummySpectrum.Add("}");
+                            File.WriteAllLines($"{Path.GetDirectoryName(args[0])}\\CustomEarwaxSounds\\SpectrumDummies\\{audio.ID}.jet", DummySpectrum);
+                        }
                     }
 
-                    // Remove the now useless WAV.
-                    File.Delete($@"{Path.GetDirectoryName(args[0])}\custom_{i}.wav");
+                    JsonSerializerSettings serializerSettings = new()
+                    {
+                        ContractResolver = new CamelCasePropertyNamesContractResolver()
+                    };
+                    File.WriteAllText($"{Path.GetDirectoryName(args[0])}\\CustomEarwaxSounds\\{Path.GetFileNameWithoutExtension(args[0])}.jet", JsonConvert.SerializeObject(AudioData, Formatting.Indented, serializerSettings));
                 }
             }
 
-
-            // Write the JET file, patching the capitalisation problem on PromptAudio.
-            JsonSerializerSettings serializerSettings = new()
+            // Check if this is a prompt list.
+            if (text[0] == "[Prompts]")
             {
-                ContractResolver = new CamelCasePropertyNamesContractResolver()
-            };
-            File.WriteAllText($"{Path.GetDirectoryName(args[0])}\\{Path.GetFileNameWithoutExtension(args[0])}.jet", JsonConvert.SerializeObject(Data, Formatting.Indented, serializerSettings));
+                // Create a directory to store things.
+                // TODO, maybe see about checking if this already exists and asking for permission to delete it.
+                Directory.CreateDirectory($"{Path.GetDirectoryName(args[0])}\\CustomEarwaxPrompts");
 
-            string[] dumbJSONWorkaround = File.ReadAllLines($"{Path.GetDirectoryName(args[0])}\\{Path.GetFileNameWithoutExtension(args[0])}.jet");
-            for (int i = 0; i < dumbJSONWorkaround.Length; i++)
-            {
-                dumbJSONWorkaround[i] = dumbJSONWorkaround[i].Replace("promptAudio", "PromptAudio");
+                // Set up the actual Data.
+                PromptFormatData Data = new();
+
+                // Loop through each entry in our text file.
+                for (int i = 1; i < text.Length; i++)
+                {
+                    Console.WriteLine($"Converting '{text[i]}' to prompt.");
+
+                    EarwaxPrompt prompt = new()
+                    {
+                        ID = 24000000 + i,
+                        X = false,
+                        PromptAudio = $"custom_{i}",
+                        Name = text[i]
+                    };
+
+                    Data.Content.Add(prompt);
+
+                    // Create the WAV for the user to manually make into OGGs (src: https://docs.microsoft.com/en-us/dotnet/api/system.speech.synthesis.speechsynthesizer.setoutputtowavefile?view=netframework-4.8)
+                    // Initialize a new instance of the SpeechSynthesizer.  
+                    SpeechSynthesizer synth = new();
+
+                    // Configure the audio output.   
+                    synth.SetOutputToWaveFile($@"{Path.GetDirectoryName(args[0])}\CustomEarwaxPrompts\custom_{i}.wav");
+
+                    // Build a prompt, strip out the control tags.  
+                    PromptBuilder builder = new();
+                    string tts = text[i].Replace("<ANY>", "this player");
+                    tts = tts.Replace("<i>", "");
+                    tts = tts.Replace("</i>", "");
+                    builder.AppendText(tts);
+
+                    // Speak the sound to our WAV file.
+                    synth.Speak(builder);
+
+                    // Dispose of the builder so we can actually delete the WAV afterwards.
+                    synth.Dispose();
+
+                    // Convert WAV to OGG
+                    if (File.Exists($@"{Path.GetDirectoryName(args[0])}\CustomEarwaxPrompts\custom_{i}.wav"))
+                    {
+                        using (Process process = new())
+                        {
+                            process.StartInfo.FileName = $"\"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\\ExternalResources\\oggenc2.exe\"";
+                            process.StartInfo.Arguments = $"\"{Path.GetDirectoryName(args[0])}\\CustomEarwaxPrompts\\custom_{i}.wav\"";
+                            process.StartInfo.UseShellExecute = false;
+                            process.StartInfo.RedirectStandardOutput = true;
+                            process.StartInfo.CreateNoWindow = true;
+
+                            process.Start();
+                            process.BeginOutputReadLine();
+                            process.WaitForExit();
+                        }
+
+                        // Remove the now useless WAV.
+                        File.Delete($@"{Path.GetDirectoryName(args[0])}\CustomEarwaxPrompts\custom_{i}.wav");
+                    }
+                }
+
+
+                // Write the JET file, patching the capitalisation problem on PromptAudio.
+                JsonSerializerSettings serializerSettings = new()
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+                File.WriteAllText($"{Path.GetDirectoryName(args[0])}\\CustomEarwaxPrompts\\{Path.GetFileNameWithoutExtension(args[0])}.jet", JsonConvert.SerializeObject(Data, Formatting.Indented, serializerSettings));
+
+                string[] dumbJSONWorkaround = File.ReadAllLines($"{Path.GetDirectoryName(args[0])}\\CustomEarwaxPrompts\\{Path.GetFileNameWithoutExtension(args[0])}.jet");
+                for (int i = 0; i < dumbJSONWorkaround.Length; i++)
+                {
+                    dumbJSONWorkaround[i] = dumbJSONWorkaround[i].Replace("promptAudio", "PromptAudio");
+                }
+                File.WriteAllLines($"{Path.GetDirectoryName(args[0])}\\CustomEarwaxPrompts\\{Path.GetFileNameWithoutExtension(args[0])}.jet", dumbJSONWorkaround);
             }
-            File.WriteAllLines($"{Path.GetDirectoryName(args[0])}\\{Path.GetFileNameWithoutExtension(args[0])}.jet", dumbJSONWorkaround);
+
+            // See if it is actually either.
+            if (text[0] != "[Audio]" && text[0] != "[Prompts]")
+            {
+                Console.WriteLine($"'{args[0]}' does not appear to be either an Audio List or a Prompt List.\nPress any key to continue.");
+                Console.ReadKey();
+                return;
+            }
+
+            // Tell the user we're done.
+            Console.WriteLine("\nDone!\nPress any key to continue.");
+            Console.ReadKey();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # Earwax Prompt Importer
-Very sloppy cmd app that converts plain text into a prompt file for the game Earwax in The Jackbox Party Pack 2. To use, simply build the project then drag a text file onto the resulting executable to get a `.jet` file (really just a `.json` file) that can replace the original `EarwaxPrompts.jet` file in your Earwax content directory (`{INSTALLDIR}\The Jackbox Party Pack 2\games\Earwax\content`). This will also create OGG files which are simple Text to Speech generations of the prompt text. Place these into the EarwaxPrompts directory (`{INSTALLDIR}\The Jackbox Party Pack 2\games\Earwax\content\EarwaxPrompts`).
+Very sloppy cmd app that converts plain text into a prompt file or a list of audio for the game Earwax in The Jackbox Party Pack 2. To use, simply build the project then drag a text file onto the resulting executable.
+
+
+### Prompts
+For Prompts, the text file file needs to begin with `[Prompt]` then have each prompt seperated by a line break. Prompt Lists generate a `.jet` file (really just a `.json` file) insde a new `CustomEarwaxPrompts` directory that can replace the original `EarwaxPrompts.jet` file in your Earwax content directory (`{INSTALLDIR}\The Jackbox Party Pack 2\games\Earwax\content`). This will also create OGG files in the `CustomEarwaxPrompts` directory which are simple Text to Speech generations of the prompt text. Place these into the EarwaxPrompts directory (`{INSTALLDIR}\The Jackbox Party Pack 2\games\Earwax\content\EarwaxPrompts`).
 
 A prompt can italisice part by using the standard `<i></i>` html tags, or a player name by using an `<ANY>` tag where you want the name to show.
+
+### Sounds
+For Sounds, the text file needs to begin with `[Audio]` then have each sound's file path on a line, followed by a break character `|` then the text that should be shown to subtitle the sound. This will generate a `.jet` file (really just a `.json` file) insde a new `CustomEarwaxSounds` directory that can replace the original `EarwaxAudio.jet` file in your Earwax content directory (`{INSTALLDIR}\The Jackbox Party Pack 2\games\Earwax\content`). An OGG version of each sound will also be created in `CustomEarwaxSounds\Oggs`, these need to be copied to the Audio folder (`{INSTALLDIR}\The Jackbox Party Pack 2\games\Earwax\content\EarwaxAudio\Audio`). Dummy spectrum visualisations are also placed in `CustomEarwaxSounds\SpectrumDummies`, these need to be copied to the Spectrum folder (`{INSTALLDIR}\The Jackbox Party Pack 2\games\Earwax\content\EarwaxAudio\Spectrum`).
+
+### File Examples
+```
+[Prompt]
+Demonstration Prompt 1
+Demonstration Prompt 2, using <i>Italic</t> text.
+Demonstration Prompt 3, using <ANY>'s name.
+```
+
+```
+[Audio]
+Z:\DemoSound1.wav|Demonstration Sound 1
+Z:\DemoSound2.ogg|Demonstration Sound 2
+```
